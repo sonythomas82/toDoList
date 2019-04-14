74b4a4e9f10332f4272f8988bcec4cbb1f5049f9
STOM <sonythomas82@gmail.com>
2019-04-13 22:40:24 -0500

    Adding LocalStorage to save list item

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,13 +9,22 @@ import { DataProvider } from '../../providers/data/data';
 })
 export class HomePage {
 
-  homelists:any;
-
-  constructor(public navCtrl: NavController, public data:DataProvider) {
+  // homelists: any;
+  newItem: any;
+  constructor(public navCtrl: NavController, public data: DataProvider) {
 
   }
 
-  ionViewDidLoad(){
-    this.homelists = this.data.lists
+  // ionViewDidLoad() {
+    // this.homelists = this.data.lists;
+  // }
+
+  addItem() {
+    if (this.newItem != "") {
+      // console.log('Befoore:' +JSON.stringify(this.data.lists));
+      this.data.lists[this.data.lists.length] = { item: this.newItem };
+      this.newItem = "";
+      this.data.saveItem(this.newItem);
+    }
   }
 }
